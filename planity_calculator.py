361ea0f0f252e72883e8b002996a237c5f257790
nicolasplantier <nicolas.plantier@mines-paristech.fr>
2023-01-16 11:53:52 +0100

    it should work with pandas now

--- planity_calculator.py
+++ planity_calculator.py
@@ -13,51 +13,39 @@ import colorcet as cc
 from statsmodels.formula.api import ols 
 from tqdm import tqdm 
 import time
+import math 
 
 
 # Definning global variables
 radius = 0.06
+px_size = 0.06
 radius_l2 = 0.05
 radius2 = 1
 
 
 # Definition of the planity_measure 
-def planity_measure(point, radius):
+def planity_measure_df(df):
     """This function takes a point as input and gives the absolute error : distance to the plane at this point"""
-    mask_x = (radius > point['x'] - df_final['x']) & (radius > df_final['x'] - point['x'])
-    mask_y = (radius > point['y'] - df_final['y']) & (radius > df_final['y'] - point['y'])
-    mask_z = (radius > point['z'] - df_final['z']) & (radius > df_final['z'] - point['z'])
-    mask_neighboorhood = mask_x &  mask_y & mask_z
-    
-    #mask_neighboorhood = ((((df_final[['x', 'y', 'z']] - point)**2).sum(axis = 1)) < radius_l2)
-    neighborhood = df_final[mask_neighboorhood]
-
-    if len(neighborhood) <= 3:
+    if len(df) <= 3:
         planity_measure = 1
     else :
-        regress_plane = ols("z ~ x + y", neighborhood).fit()
+        regress_plane = ols("z ~ x + y", df).fit()
         d, a, b = regress_plane._results.params
         c = -1
         normal = np.array([a,b,c])
         normal = normal/np.linalg.norm(normal)
         cos_theta = np.abs(normal[2])
-        z_predict = regress_plane.predict(neighborhood)
-        # planity_measure = np.abs((cos_theta*(np.abs(z_predict - neighborhood['z']))).mean())
-        dist_table = cos_theta*(np.abs(z_predict - neighborhood['z']))
+        z_predict = regress_plane.predict(df)
+        dist_table = cos_theta*(np.abs(z_predict - df['z']))
         dist_table_max = dist_table.copy()
         ids = []
-        for k in range(len(neighborhood)//10): 
+        for k in range(len(df)//10): 
             id_max = dist_table_max.idxmax(axis = 0)
             ids.append(id_max)
             dist_table_max.drop(index = id_max)
         dist_table.loc[ids] = 100*dist_table[ids]
         planity_measure = dist_table.std()
-        #print(planity_measure)
-
-    index = df_final[mask_neighboorhood].index
-    df_final.loc[index,'planity_classifier'] = planity_measure
-
-    return index
+    return planity_measure
     
 
 if __name__ == "__main__":
@@ -79,28 +67,32 @@ if __name__ == "__main__":
     np_coords_3d = np.concatenate((x_scaled.reshape((n,1)), y_scaled.reshape((n,1)), z_scaled.reshape((n,1))), axis = 1)
     df_coords_3d = pd.DataFrame(data=np_coords_3d, columns=['x', 'y', 'z']) 
 
+    # We create a new method, where we calculate the planity measure on voxels 
+    xmax, xmin = np.max(df_coords_3d['x']), np.min(df_coords_3d['x'])
+    ymax, ymin = np.max(df_coords_3d['y']), np.min(df_coords_3d['y'])
+    zmax, zmin = np.max(df_coords_3d['z']), np.min(df_coords_3d['z'])
+    N_x = math.floor((xmax - xmin)/px_size)
+    N_y = math.floor((ymax - ymin)/px_size)
+    N_z = math.floor((zmax - zmin)/px_size)
+    indexs = df_coords_3d.index
+    df_intermediate = df_coords_3d.copy()
 
-    # We create the intermediate dataframes to calculate the planity measure 
-    df_to_be_classified = df_coords_3d.copy()
-    df_final = df_coords_3d.copy()
-    df_to_be_classified['planity_classifier'] = 0
-    df_final['planity_classifier'] = 0
-
-    initial_lengh = len(df_to_be_classified)
-    while len(df_to_be_classified) > 1:
-        print(np.round(100*(1 - len(df_to_be_classified)/initial_lengh),2), end = "\r")
-        point = df_to_be_classified.iloc[0]
-        index = planity_measure(point, radius = radius)
-        df2 = df_final.loc[index]
-        final_index = pd.merge(df_to_be_classified, df2, left_index=True, right_index=True).index
-        df_to_be_classified.drop(final_index, axis= 0, inplace = True)
+    # Let's know where each point belongs (voxel number)
+    df_intermediate['i'] = (N_x*((df_coords_3d['x'] - xmin)/(xmax - xmin))).astype(np.int)
+    df_intermediate['j'] = (N_y*((df_coords_3d['y'] - ymin)/(ymax - ymin))).astype(np.int)
+    df_intermediate['k'] = (N_z*((df_coords_3d['z'] - zmin)/(zmax - zmin))).astype(np.int)
+    df_intermediate['voxel'] = df_intermediate['i'] + df_intermediate['j']*(np.log10(N_x)//1 + 1) + df_intermediate['k']*(np.log10(N_x)//1 + np.log10(N_y)//1 + 2)
+    
+    # We calculate the planity measure in each voxel 
+    data = df_intermediate.groupby(['voxel'], group_keys=True).apply(planity_measure_df)
+    df_final = pd.merge(df_intermediate, data.to_frame(name = "planity_measure"), left_on='voxel', right_index=True, inplace = True)
 
     # What time is it ? 
     print(np.round(time.time() - start), 2)
 
     index = df_final.index
     las.add_extra_dim(laspy.ExtraBytesParams(name="planity",type=np.float64,description="More classes available"))
-    las.points.planity[:] = df_final['planity_classifier']
+    las.points.planity[:] = df_final['planity_measure']
 
     las.write('3d_patch_classified_test.las')
 
